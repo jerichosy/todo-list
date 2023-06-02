56355f0854bfb91631ae265a55b30c8c5c596899
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2023-06-02 10:04:33 +0800

    Hard coded subtasks working

--- src/app/components/Todo.js
+++ src/app/components/Todo.js
@@ -1,12 +1,30 @@
 'use client';
 
+import TodoSub from "./TodoSub";
+
 export default function Todo({ id, title, done, handleDeleteTodo }) {
+    const subTasks = [
+        { id: 1, title: 'subtask 1', done: false },
+        { id: 2, title: 'subtask 2', done: false }
+    ]
+
     return (
         <>
             <input type="checkbox" checked={done} />
             <input type="text" value={title} disabled={true} />
             {/* <span>{todo.title}</span> */}
             <button type='button' onClick={() => handleDeleteTodo(id)}>Delete</button>
+            <ul>
+                {subTasks.map(subTask => (
+                    <li key={subTask.id}>
+                        <TodoSub
+                            id={subTask.id}
+                            title={subTask.title}
+                            done={subTask.done}
+                        />
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
---
+++ src/app/components/TodoSub.js
@@ -0,0 +1,11 @@
+export default function TodoSub( {id, title, done} ) {
+    return (
+        <>
+            <input type="checkbox" checked={done} />
+            <input type="text" value={title} disabled={true}/>
+            {/* <span>{todo.title}</span> */}
+            {/* <button type='button' onClick={() => handleDeleteTodo(id)}>Delete</button> */}
+        </>
+    )
+
+}
