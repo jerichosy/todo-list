6021027983bb8821e86d386163d8cc0d9bffe0e9
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2023-06-02 09:59:53 +0800

    Update Todo.js (formatting)

--- src/app/components/Todo.js
+++ src/app/components/Todo.js
@@ -4,7 +4,7 @@ export default function Todo({ id, title, done, handleDeleteTodo }) {
     return (
         <>
             <input type="checkbox" checked={done} />
-            <input type="text" value={title} disabled={true}/>
+            <input type="text" value={title} disabled={true} />
             {/* <span>{todo.title}</span> */}
             <button type='button' onClick={() => handleDeleteTodo(id)}>Delete</button>
         </>
